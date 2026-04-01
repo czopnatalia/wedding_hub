57f55a0a1b9ed6dd3fd25f574a6a7d632e59081c
czopnatalia <nczop10@gmail.com>
2026-04-01 20:15:34 +0200

    styslistyka strony

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,7 @@
-</div>
-<footer>
+</div> </div>
+<footer style="text-align: center; padding: 18px; color: var(--text-main); font-size: 0.85rem; margin-top: auto;">
     Natalia i Łukasz © 2026
 </footer>
+
 </body>
 </html>
